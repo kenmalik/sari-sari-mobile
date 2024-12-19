d3fab1808d7d914b4a6a4f1c26462c1cb0d22c86
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-12-19 15:09:40 -0800

    Make constants for featured collection IDs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
 import { CollectionPreview } from "@/components/CollectionPreview";
 import { ScrollView } from "react-native";
 
+const BEST_SELLERS_COLLECTION_ID = "gid://shopify/Collection/260817223773";
+const NEW_ARRIVALS_COLLECTION_ID = "gid://shopify/Collection/273118527581";
+
 export default function Index() {
   return (
     <ScrollView>
-      <CollectionPreview collectionId="gid://shopify/Collection/260817223773" />
-      <CollectionPreview collectionId="gid://shopify/Collection/273118527581" />
+      <CollectionPreview collectionId={BEST_SELLERS_COLLECTION_ID} />
+      <CollectionPreview collectionId={NEW_ARRIVALS_COLLECTION_ID} />
     </ScrollView>
   );
 }
